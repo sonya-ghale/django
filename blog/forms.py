6c504113ac6bf3aae1580fd5c8976a6f56c15b85
sonya-ghaleg36@gmail.com <roshmitamang36@gmail.com>
2025-06-30 17:06:28 +0545

    Added views to create/edit blog post

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+
+from .models import Post 
+
+# forms.ModelForm is responsible for telling django that this form is a model form
+class PostForm(forms.ModelForm): 
+
+# tell django which model should be created from this form
+    class Meta:
+        model = Post
+        fields =('title', 'text')
--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # the last name='posy_list' is the name of the url that will be used to identify the view 
     #path('', views.post_list, name='post_list'),
+
+    path('post/new/', views.post_new, name='post_new'),
+
+    path('post/<int:pk>/edit/', views.post_edit, name="post_edit"),
 ]
 
